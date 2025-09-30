7f7e13caddd357ce24588e700c27fc9f0cef1c66
marky <markrhonaldbodegon@gmail.com>
2025-09-30 15:41:19 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,9 @@
+import unittest
+from app.main import add
 
+class TestApp(unittest.TestCase);
+  def test_add(self):
+    self.assertEqual(add(2, 3), 5)
+
+if _name_ == "_main_":
+  unittest.main()
